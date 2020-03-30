1e2ffe66357a1da4af0785e30f3aa5f3c4b822c9
Travis Chapman <traviswchapman@gmail.com>
2020-03-30 13:23:33 -0400

    Detect Sundays

--- index.js
+++ index.js
@@ -11,17 +11,28 @@ bot.on('ready', () => {
     console.log(bot.user.tag, 'listening for turnip prices.');
 })
 
+let prices = [];
+let lastDay;
 bot.on('message', message => {
     if (!message.content.startsWith(command)) {
         return;
     }
 
-    const value = message.content.substring(command.length);
-    const valueInt = parseInt(value)
+    const date = message.createdAt;
+    if (date.getDay() !== lastDay) {
+        prices = [];
+        lastDay = date.getDay();
+    }
+
+    const param = message.content.substring(command.length).trim();
+
+    const valueInt = parseInt(param)
     if (isNaN(valueInt)) {
         message.reply(`${valueInt} is not an integer, and therefore not a valid price.`);
         return;
     }
+    const isBuyPrice = date.getDay() !== 0;
+    const boughtOrSold = isBuyPrice ? 'bought' : 'sold';
 
-    message.reply(`current price is ${valueInt}`);
+    message.channel.send(`Turnips are being ${boughtOrSold} on ${message.member.user.username}'s island for **${valueInt}** bells.`);
 })
